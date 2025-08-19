fdfc52005cc5dd5cb95123aaf0e72ae950c29f84
vision39 <dkrray772@gmail.com>
2025-08-19 17:57:04 +0530

    Added Example

---
+++ config-example.py
@@ -0,0 +1,31 @@
+# for tortoise-orm
+
+TORTOISE = {}
+
+
+POSTGRESQL = {}
+
+EXTENSIONS = ()
+
+DISCORD_TOKEN = ""
+
+COLOR = 0x00d9ff
+
+FOOTER = "ME is here!"
+
+PREFIX = "m"
+
+SERVER_LINK = ""
+
+BOT_INVITE = ""
+
+WEBSITE = ""
+
+REPOSITORY = ""
+
+DEVS = ()
+
+# LOGS
+SHARD_LOG = ""
+ERROR_LOG = ""
+PUBLIC_LOG = ""
